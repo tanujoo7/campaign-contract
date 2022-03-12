e903349c9c9f79292a23ce86d41b998507d2eadf
Tanuj <tanujg93@gmail.com>
2022-03-12 11:41:58 +0530

    updates

--- deploy.js
+++ deploy.js
@@ -22,17 +22,5 @@ const deploy = async () => {
   console.log('Contract deployed to', result.options.address);
   provider.engine.stop();
 };
-const undeploy = async () => {
-  const accounts = await web3.eth.getAccounts();
 
-  console.log('Attempting to deploy from account', accounts[0]);
-
-  const result = await new web3.eth.Contract(abi)
-    .deploy({ data: evm.bytecode.object })
-    .send({ gas: '1000000', from: accounts[0] });
-
-  console.log(JSON.stringify(abi));
-  console.log('Contract deployed to', result.options.address);
-  provider.engine.stop();
-};
 deploy();
